13e2290594c9399182a2d0596afbe0d9879e8939
nisarg281 <124192085+nisarg281@users.noreply.github.com>
2024-06-19 07:29:54 +0000

    lecture2 for data types basics

---
+++ basics/02_DataTypes.js
@@ -0,0 +1,36 @@
+// Javascript is updated a lot more. So to use the newer syntaxes only, use below declaration.
+"use strict";   // In most of cases , this is by default. 
+
+// alert("warrningggggg")    // Cause error because we're using nodejs, not browser.   
+/* This will show a popup notification when used in inspect console. 
+ But here, it'll throw a warning message because syntax is wrong. */
+
+// Data types : 
+// number => range : 2^53 around. 
+// bigint 
+// string =>  " "
+// boolean => true / false
+
+// null => stadalone value , represents emptyness of data. 
+// For example, if temperature website has no data about temp., then they can't return 0 because 0 is also a temperature. So they return null.
+
+// undefined => value is not assinged to variable.
+
+// symbol => to find uniqueness. used in react.
+
+// object (a non-primitive data type).
+
+let name1 ="nisarg"
+let nisarg=12
+
+console.log(typeof name1)
+console.log(typeof "nisarg");
+console.log(typeof nisarg);
+
+console.log(typeof null); // Gives answer as object. 
+console.log(typeof undefined); // Gives answer as undefined. 
+
+//----------- Undefined is type but null is an object. -------------  // 
+
+
+
